50fbc6c1d95db34d0428ea733912885842f689c2
lucashdez <lukastpiano@gmail.com>
2022-11-20 15:17:24 +0000

    Python null-ls

--- plugin/null-ls.rc.lua
+++ plugin/null-ls.rc.lua
@@ -1,63 +1,64 @@
 local status, null_ls = pcall(require, "null-ls")
 if not status then
-  return
+	return
 end
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local lsp_formatting = function(bufnr)
-  vim.lsp.buf.format({
-    filter = function(client)
-      return client.name == "null-ls"
-    end,
-    bufnr = bufnr,
-  })
+	vim.lsp.buf.format({
+		filter = function(client)
+			return client.name == "null-ls"
+		end,
+		bufnr = bufnr,
+	})
 end
 
 null_ls.setup({
-  debug = true,
-  sources = {
-    null_ls.builtins.formatting.prettierd.with({
-      extra_args = {
-        "--tab-width=2",
-      },
-    }),
-    null_ls.builtins.diagnostics.eslint_d.with({
-      diagnostics_format = "[eslint] #{m}\n(#{c})",
-    }),
-    null_ls.builtins.diagnostics.fish,
+	debug = true,
+	sources = {
+		null_ls.builtins.formatting.prettierd.with({
+			extra_args = {
+				"--tab-width=2",
+			},
+		}),
+		null_ls.builtins.diagnostics.eslint_d.with({
+			diagnostics_format = "[eslint] #{m}\n(#{c})",
+		}),
+		null_ls.builtins.diagnostics.fish,
 
-    --CODE ACTIONS
-    null_ls.builtins.code_actions.ltrs,
+		--CODE ACTIONS
+		null_ls.builtins.code_actions.ltrs,
 
-    --LINTER
-    null_ls.builtins.diagnostics.ltrs,
-    null_ls.builtins.diagnostics.cpplint,
+		--LINTER
+		null_ls.builtins.diagnostics.ltrs,
+		null_ls.builtins.diagnostics.cpplint,
 
-    --FORMAT
-    null_ls.builtins.formatting.rustfmt.with({
-      extra_args = { "--edition=2021" },
-    }),
-    null_ls.builtins.formatting.clang_format,
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.csharpier,
-    --------------
-  },
+		--FORMAT
+		null_ls.builtins.formatting.rustfmt.with({
+			extra_args = { "--edition=2021" },
+		}),
+		null_ls.builtins.formatting.clang_format,
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.csharpier,
+		null_ls.builtins.formatting.blue,
+		--------------
+	},
 
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          lsp_formatting(bufnr)
-        end,
-      })
-    end
-  end,
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					lsp_formatting(bufnr)
+				end,
+			})
+		end
+	end,
 })
 
 vim.api.nvim_create_user_command("DisableLspFormatting", function()
-  vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
+	vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
 end, { nargs = 0 })
