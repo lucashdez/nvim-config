89755dcefb41201323556cec1837b2b5f1f58adc
lucashdez <lukastpiano@gmail.com>
2022-11-20 12:38:49 +0000

    Added git support

--- lua/craftzdog/maps.lua
+++ lua/craftzdog/maps.lua
@@ -16,9 +16,6 @@ keymap.set("n", ";q", "<Cmd>q<CR>")
 keymap.set("n", "+", "<C-a>")
 keymap.set("n", "-", "<C-x>")
 
--- Delete a word backwards
-keymap.set("n", "dw", 'vb"_d')
-
 -- Select all
 keymap.set("n", "<C-a>", "gg<S-v>G")
 
@@ -26,7 +23,7 @@ keymap.set("n", "<C-a>", "gg<S-v>G")
 --vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
 
 -- New tab
-keymap.set("n", "te", ":tabedit")
+keymap.set("n", ";te", ":tabedit")
 -- Split window
 keymap.set("n", "ss", ":split<Return><C-w>w")
 keymap.set("n", "sv", ":vsplit<Return><C-w>w")
@@ -65,3 +62,12 @@ keymap.set("n", ";g", "<Cmd>noh<CR>")
 
 -- TODO COMMENTS
 keymap.set("n", ";td", "<Cmd>TODOTelescope<CR>")
+
+-- NO ARROWS
+keymap.set("n", "<Up>", "<Nop>")
+keymap.set("n", "<Down>", "<Nop>")
+keymap.set("n", "<Left>", "<Nop>")
+keymap.set("n", "<Right>", "<Nop>")
+
+-- GIT
+keymap.set("n", ";gb", "<Cmd>GitBlame<CR>")
