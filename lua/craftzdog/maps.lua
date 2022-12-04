fc459029df80013ff49237d9354393798c739400
lucashdez <lukastpiano@gmail.com>
2022-12-04 10:29:50 +0000

    Added comfy esc

--- lua/craftzdog/maps.lua
+++ lua/craftzdog/maps.lua
@@ -77,5 +77,5 @@ keymap.set("n", ";gg", ":Git ")
 -- FOLD
 keymap.set("n", ";<Space>", "za")
 -- Comfy Esc
-keymap.set("i", "<S-Tab>", "<Esc>")
-keymap.set("v", "<S-Tab>", "<Esc>")
+keymap.set("i", "<C-k>", "<Esc>")
+keymap.set("v", "<C-k>", "<Esc>")
