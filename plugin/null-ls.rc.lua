e2353a17aaf82a0a7fb29a7a33f364d132e2ff7c
lucashdez <lukastpiano@gmail.com>
2022-12-15 10:08:32 +0000

    Added 4 spaces

--- plugin/null-ls.rc.lua
+++ plugin/null-ls.rc.lua
@@ -1,70 +1,69 @@
 local status, null_ls = pcall(require, "null-ls")
 if not status then
-    return
+	return
 end
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local lsp_formatting = function(bufnr)
-    vim.lsp.buf.format({
-        filter = function(client)
-            return client.name == "null-ls"
-        end,
-        bufnr = bufnr,
-    })
+	vim.lsp.buf.format({
+		filter = function(client)
+			return client.name == "null-ls"
+		end,
+		bufnr = bufnr,
+	})
 end
 
 null_ls.setup({
-    debug = true,
-    sources = {
-        null_ls.builtins.formatting.prettierd.with({
-            extra_args = {
-                "--tab-width=4",
-            },
-        }),
-        null_ls.builtins.diagnostics.fish,
+	debug = true,
+	sources = {
+		null_ls.builtins.formatting.prettierd.with({
+			extra_args = {
+				"--tab-width=4",
+			},
+		}),
+		null_ls.builtins.diagnostics.fish,
 
-        --CODE ACTIONS
-        null_ls.builtins.code_actions.ltrs,
+		--CODE ACTIONS
+		null_ls.builtins.code_actions.ltrs,
 
-        --LINTER
-        null_ls.builtins.diagnostics.ltrs,
+		--LINTER
+		null_ls.builtins.diagnostics.ltrs,
 
-        --FORMAT
-        null_ls.builtins.formatting.rustfmt.with({
-            extra_args = {
-                "--edition=2021",
-                "--config",
-                "brace_style=AlwaysNextLine,control_brace_style=AlwaysNextLine",
-            },
-        }),
-        null_ls.builtins.formatting.clang_format.with({
-            extra_args = {
-                "--style",
-                -- IndentWidth: 4
-                "{ TabWidth: 4, BreakBeforeBraces: Allman, AlwaysBreakAfterReturnType: TopLevelDefinitions, IndentCaseLabels: true}",
-            },
-        }),
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.csharpier,
-        null_ls.builtins.formatting.blue,
-        --------------
-    },
+		--FORMAT
+		null_ls.builtins.formatting.rustfmt.with({
+			extra_args = {
+				"--edition=2021",
+				"--config",
+				"brace_style=AlwaysNextLine,control_brace_style=AlwaysNextLine",
+			},
+		}),
+		null_ls.builtins.formatting.clang_format.with({
+			extra_args = {
+				"--style",
+				"{ IndentWidth: 4, TabWidth: 4, BreakBeforeBraces: Allman, AlwaysBreakAfterReturnType: TopLevelDefinitions, IndentCaseLabels: true}",
+			},
+		}),
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.csharpier,
+		null_ls.builtins.formatting.blue,
+		--------------
+	},
 
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    lsp_formatting(bufnr)
-                end,
-            })
-        end
-    end,
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					lsp_formatting(bufnr)
+				end,
+			})
+		end
+	end,
 })
 
 vim.api.nvim_create_user_command("DisableLspFormatting", function()
-    vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
+	vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
 end, { nargs = 0 })
