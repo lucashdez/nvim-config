d568bdd13d9046f84c6e60da1f7a6ed1b872fb7b
lucashdez <lukastpiano@gmail.com>
2024-07-24 16:57:39 +0100

    Added markdown

--- init.lua
+++ init.lua
@@ -878,6 +878,51 @@ require('lazy').setup({
       require('startup').setup { theme = 'startify' }
     end,
   },
+  {
+    'MeanderingProgrammer/markdown.nvim',
+    main = 'render-markdown',
+    opts = {},
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('render-markdown').setup {
+        heading = {
+          -- Turn on / off heading icon & background rendering
+          enabled = true,
+          -- Turn on / off any sign column related rendering
+          sign = true,
+          -- Replaces '#+' of 'atx_h._marker'
+          -- The number of '#' in the heading determines the 'level'
+          -- The 'level' is used to index into the array using a cycle
+          -- The result is left padded with spaces to hide any additional '#'
+          icons = { '󰲡 ', '󰲣 ', '󰲥 ', '󰲧 ', '󰲩 ', '󰲫 ' },
+          -- Added to the sign column if enabled
+          --
+          -- The 'level' is used to index into the array using a cycle
+          signs = { '󰫎 ' },
+          -- The 'level' is used to index into the array using a clamp
+          -- Highlight for the heading icon and extends through the entire line
+          backgrounds = {
+            'RenderMarkdownH1Bg',
+            'RenderMarkdownH2Bg',
+            'RenderMarkdownH3Bg',
+            'RenderMarkdownH4Bg',
+            'RenderMarkdownH5Bg',
+            'RenderMarkdownH6Bg',
+          },
+          -- The 'level' is used to index into the array using a clamp
+          -- Highlight for the heading and sign icons
+          foregrounds = {
+            'RenderMarkdownH1',
+            'RenderMarkdownH2',
+            'RenderMarkdownH3',
+            'RenderMarkdownH4',
+            'RenderMarkdownH5',
+            'RenderMarkdownH6',
+          },
+        },
+      }
+    end,
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
@@ -927,5 +972,7 @@ vim.cmd [[highlight nlCursor guifg=fg guibg=#dd4040]]
 vim.cmd [[highlight ilCursor guifg=fg guibg=#40FF40]]
 vim.o.guicursor = 'n-v-c:block-nlCursor,i-ci-ve:ver25-ilCursor'
 vim.cmd.colorscheme 'solarized-osaka'
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
