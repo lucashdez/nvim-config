d803a35a174dd132aec86827f3b53dd38bb6f7fb
lucashdez <lukastpiano@gmail.com>
2025-03-12 16:31:01 +0000

    Added cssls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -33,6 +33,13 @@ return {
             lspconfig.zls.setup({
                 capabilities = capabilities,
             })
+            lsp.cssls.setup({
+                capabilities = capabilities,
+            })
+
+            lsp.denols.setup({
+                capabilities = capabilities,
+            })
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
