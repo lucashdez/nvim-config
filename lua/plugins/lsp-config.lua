213d1cdae55cf40b3cce348b0d67ed1f16e0935b
lucashdez <lukastpiano@gmail.com>
2025-06-16 18:33:52 +0100

    Added csharp-ls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -45,6 +45,10 @@ return {
                 capabilities = capabilities,
             }
 
+            lspconfig.csharp_ls.setup {
+                capabilities = capabilities,
+            }
+
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, {})
