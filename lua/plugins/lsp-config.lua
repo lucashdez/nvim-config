084f0d20d5307045527e9f126f3e0c5578ddcd7b
lucashdez <lukastpiano@gmail.com>
2025-09-13 10:42:30 +0100

    a

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -59,6 +59,10 @@ return {
                 capabilities = capabilities,
             }
 
+            lspconfig.pyright.setup {
+                capabilities = capabilities
+            }
+
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, {})
