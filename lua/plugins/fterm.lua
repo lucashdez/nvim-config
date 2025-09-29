3ac7ea354cfa28842532eb8248b8e00a49632977
Lucas Hernandez Abreu <lhernandezabreu@techtitute.com>
2025-09-29 13:10:42 +0100

    Added conditional term

--- lua/plugins/fterm.lua
+++ lua/plugins/fterm.lua
@@ -2,9 +2,13 @@ return {
     {
         'numToStr/FTerm.nvim',
         config = function()
+            local cmd_shell = 'zsh'
+            if vim.fn.has("win32") then
+                cmd_shell = "nu"
+            end 
             require('FTerm').setup {
                 border = 'double',
-                cmd = 'zsh',
+                cmd = cmd_shell,
             }
             vim.keymap.set('n', '<C-t>', function()
                 require('FTerm').toggle()
